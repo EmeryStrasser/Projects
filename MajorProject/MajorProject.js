80c19cdd1c65ad0f6742400f27bd64e2b4f5ffa5
Emery <52927@newington.nsw.edu.au>
2018-05-29 10:01:30 +1000

    s

--- MajorProject/MajorProject.js
+++ MajorProject/MajorProject.js
@@ -2,27 +2,28 @@ let totalHoursDriven;
 let totalDayHours;
 let totalNightHours;
 
-let pages = document.getElementsByClassName("pages");
+// let pages = document.getElementsByClassName("pages");
 
-for (let i = 0; i < pages.length; i++) {
-  pages[i].style.display = "none";
-}
-
-let startingPage = document.getElementById("tablePage");
-
-startingPage.style.display = "block";
-
-function changePages(currentPage, newPage) {
-  const pageToHide = document.getElementById(currentPage);
-  const pageToShow = document.getElementById(newPage);
-
-  pageToHide.style.display = "none";
-  pageToShow.style.display = "block";
-}
-
-
-// function goToPage(pageNumber){
-
-// document.querySelectorAll('.pages').forEach(e)
-
+// for (let i = 0; i < pages.length; i++) {
+//   pages[i].style.display = "none";
 // }
+
+// let startingPage = document.getElementById("tablePage");
+
+// startingPage.style.display = "block";
+
+// function changePages(currentPage, newPage) {
+//   const pageToHide = document.getElementById(currentPage);
+//   const pageToShow = document.getElementById(newPage);
+
+//   pageToHide.style.display = "none";
+//   pageToShow.style.display = "block";
+// }
+goToPage(page1);
+
+function goToPage(pageNumber) {
+
+  document.querySelectorAll('.pages').forEach((e) => e.hidden = true);
+  pageNumber.hidden = false;
+
+}
