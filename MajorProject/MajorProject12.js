8ee4b7437cd5bfe6ab4d55269b337510b51b489b
Emery <52927@newington.nsw.edu.au>
2019-05-05 18:06:21 +1000

    user authenitcation

--- MajorProject/MajorProject12.js
+++ MajorProject/MajorProject12.js
@@ -6,17 +6,59 @@ let database = firebase.database();
 let UsernamePasswordRef = database.ref('/UsernamePassword');
 
 
+
+
 UsernamePasswordRef.once('value').then(reload);
 
 
 function reload(data) {
   if (usernamePasswordArray == null) usernamePasswordArray = []; //If there is no data in the online databse, it creates an array
   usernamePasswordArray = data.val();//Adding the incoming data into a object
+  console.log(usernamePasswordArray);
+
+  usernamePasswordArray.push(["Emery", "strasser"]);
+  UsernamePasswordRef.update(usernamePasswordArray);
+
+}
+
+
+function Login() {
+
+  let UserCheck = document.getElementById("loginUsername").value;
+
+  let passwordCheck = document.getElementById("loginPassword").value;
+  for (i = 0; i < usernamePasswordArray.length; i++) {
+    let temporaryArray = [];
+    temporaryArray = usernamePasswordArray[i];
+
+    if (UserCheck === temporaryArray[0]) {
+
+      console.log("FoundUsername");
+
+      if (passwordCheck === temporaryArray[1]) {
+
+        console.log("passwordFound")
+
+        return;
+      }
+
+    }
+
+  }
+
+  console.log("no matches found")
+}
+
+function NewMember() {
+
+
+
 
 }
 
 
 
+//UsernamePasswordRef.update(usernamePasswordArray);
 
 /** This function is used to hide all the different pages and then shows a single page based on the parameter
  Parameters: 
