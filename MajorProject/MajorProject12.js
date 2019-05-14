2ff24797ad8eeb28dfd3109beec5e479773c06b3
Emery <52927@newington.nsw.edu.au>
2019-05-14 10:51:30 +1000

    fixing shit

--- MajorProject/MajorProject12.js
+++ MajorProject/MajorProject12.js
@@ -17,6 +17,14 @@ let questionArray = [];
 let username;
 
 let questionNumber;
+
+
+let quizNumberCell;
+let quiznameCell;
+let quizDescCell;
+let quizRatingCell;
+let authorCell;
+
 // let quizArray = [[Quiz Name, Quiz Description, Time limit, [quiz ID, [Question, [Answer1, Correct/Wrong],[Answer2, Correct/Wrong],[Answer3, Correct/Wrong],[Answer4, Correct/Wrong]]]]]
 
 
@@ -25,12 +33,12 @@ quizRef.once('value').then(reloadQuiz);
 
 function reload(data) {
   if (usernamePasswordArray == null) usernamePasswordArray = []; //If there is no data in the online databse, it creates an array
-  if (quizArray == null) quizArray = [];
+
   usernamePasswordArray = data.val();//Adding the incoming data into a object
   console.log(usernamePasswordArray);
 
 
-  UsernamePasswordRef.update(usernamePasswordArray);
+  // UsernamePasswordRef.update(usernamePasswordArray);
 
 }
 
@@ -40,9 +48,27 @@ function reloadQuiz(data) {
   quizArray = data.val();//Adding the incoming data into a object
   console.log(quizArray);
 
+  // quizArray = [["test", "test"]];
+  // quizRef.update(quizArray);
+  // quizArray = ["test"];
+  document.getElementById('tbody').innerHTML = ''; //Accesing the logBook
+  var testRow = document.getElementById('tbody').insertRow(0);//Adding row to the table
 
-  quizRef.update(quizArray);
 
+  for (i = 0; i < quizArray.length; i++) { //Looping throught the array
+
+    var quizTable = document.getElementById("quizTable"); //Accesing logbooks
+    var row = quizTable.insertRow(quizTable.rows.length);//Inserting a new row
+    quizNumberCell = row.insertCell(0); //Inserting the different cells 
+    quiznameCell = row.insertCell(1);
+    quizDescCell = row.insertCell(2);
+    quizRatingCell = row.insertCell(3);
+    authorCell = row.insertCell(4);
+
+
+    fillCells(i); //calling function to fill the cells with appropiate data, takes parameter of the array index.
+    // calculateStats();//Function to calculate statistics
+  }
 }
 
 
@@ -77,9 +103,8 @@ function Login() {
 function NewMember() {
 
   let UserCheck = document.getElementById("newUsername").value;
-
   let passwordCheck = document.getElementById("newPassword").value;
-  console.log(UserCheck);
+
   for (i = 0; i < usernamePasswordArray.length; i++) {
 
     // let temporaryArray = [];
@@ -91,9 +116,9 @@ function NewMember() {
 
       return;
     }
-    else if (passwordCheck === usernamePasswordArray[i][1]) {
+    else if (UserCheck.toString().includes(" ")) {
 
-      alert("Password is already being used");
+      alert("Pleas remove spaces from username");
 
 
       return;
@@ -120,15 +145,16 @@ function quizCreation() {
   timeLimit = document.getElementById("timeLimit").value;
   console.log(timeLimit);
   questionNumber = 1;
-
+  document.getElementById("questionNumber").innerHTML = questionNumber;
 
 
 }
 
 function questionCreation() {
+  questionNumber++;
 
   document.getElementById("questionNumber").innerHTML = questionNumber;
-  questionNumber++;
+
 
   let question = document.getElementById("quizQuestion").value;
   let answer1 = document.getElementById("answer1").value;
@@ -136,10 +162,10 @@ function questionCreation() {
   let answer3 = document.getElementById("answer3").value;
   let answer4 = document.getElementById("answer4").value;
 
-  let checkbox1 = document.getElementById("Answer1Correct").value;
-  let checkbox2 = document.getElementById("Answer2Correct").value;
-  let checkbox3 = document.getElementById("Answer3Correct").value;
-  let checkbox4 = document.getElementById("Answer4Correct").value;
+  let checkbox1 = document.getElementById("Answer1Correct").checked;
+  let checkbox2 = document.getElementById("Answer2Correct").checked;
+  let checkbox3 = document.getElementById("Answer3Correct").checked;
+  let checkbox4 = document.getElementById("Answer4Correct").checked;
 
   questionArray.push([question, [answer1, checkbox1], [answer2, checkbox2], [answer3, checkbox3], [answer4, checkbox4]]);
 
@@ -148,11 +174,10 @@ function questionCreation() {
   document.getElementById("answer2").value = "";
   document.getElementById("answer3").value = "";
   document.getElementById("answer4").value = "";
-  document.getElementById("Answer1Correct").value = "";
-  document.getElementById("Answer2Correct").value = "";
-  document.getElementById("Answer3Correct").value = "";
-  document.getElementById("Answer4Correct").value = "";
-
+  document.getElementById("Answer1Correct").checked = false;
+  document.getElementById("Answer2Correct").checked = false;
+  document.getElementById("Answer3Correct").checked = false;
+  document.getElementById("Answer4Correct").checked = false;
 
 }
 
@@ -165,10 +190,10 @@ function finishQuizCreation() {
   let answer3 = document.getElementById("answer3").value;
   let answer4 = document.getElementById("answer4").value;
 
-  let checkbox1 = document.getElementById("Answer1Correct").value;
-  let checkbox2 = document.getElementById("Answer2Correct").value;
-  let checkbox3 = document.getElementById("Answer3Correct").value;
-  let checkbox4 = document.getElementById("Answer4Correct").value;
+  let checkbox1 = document.getElementById("Answer1Correct").checked;
+  let checkbox2 = document.getElementById("Answer2Correct").checked;
+  let checkbox3 = document.getElementById("Answer3Correct").checked;
+  let checkbox4 = document.getElementById("Answer4Correct").checked;
 
   questionArray.push([question, [answer1, checkbox1], [answer2, checkbox2], [answer3, checkbox3], [answer4, checkbox4]]);
 
@@ -177,23 +202,63 @@ function finishQuizCreation() {
   document.getElementById("answer2").value = "";
   document.getElementById("answer3").value = "";
   document.getElementById("answer4").value = "";
-  document.getElementById("Answer1Correct").value = "";
-  document.getElementById("Answer2Correct").value = "";
-  document.getElementById("Answer3Correct").value = "";
-  document.getElementById("Answer4Correct").value = "";
+  document.getElementById("Answer1Correct").checked = false;
+  document.getElementById("Answer2Correct").checked = false;
+  document.getElementById("Answer3Correct").checked = false;
+  document.getElementById("Answer4Correct").checked = false;
 
-
+  console.log(quizArray);
+  // quizArray.push("test");
   quizArray.push([quizName, quizDescription, timeLimit, username, questionArray]);
 
   questionArray = [];
 
   quizRef.update(quizArray);
 
+
+  document.getElementById('tbody').innerHTML = ''; //Accesing the logBook
+  var testRow = document.getElementById('tbody').insertRow(0);//Adding row to the table
+
+
+  for (i = 0; i < quizArray.length; i++) { //Looping throught the array
+
+    var quizTable = document.getElementById("quizTable"); //Accesing logbooks
+    var row = quizTable.insertRow(quizTable.rows.length);//Inserting a new row
+    quizNumberCell = row.insertCell(0); //Inserting the different cells 
+    quiznameCell = row.insertCell(1);
+    quizDescCell = row.insertCell(2);
+    quizRatingCell = row.insertCell(3);
+    authorCell = row.insertCell(4);
+
+
+    fillCells(i); //calling function to fill the cells with appropiate data, takes parameter of the array index.
+    // calculateStats();//Function to calculate statistics
+  }
+
   goToPage(quizPage);
 
   console.log(quizArray);
 
 }
+
+
+
+function fillCells(currentRow) {
+
+  quizNumberCell.innerHTML = "<i class='fas fa-play w3-button w3-xxlarge w3-round-xlarge' onclick='deleteRow(" + currentRow + ")'></i>"
+  //Accesing the cell and filling in the cell with values from the array and the objects
+  console.log(quizArray[currentRow][0]);
+  quiznameCell.innerHTML = quizArray[currentRow][0];
+  quizDescCell.innerHTML = quizArray[currentRow][1];
+  quizRatingCell.innerHTML = "";
+  authorCell.innerHTML = quizArray[currentRow][3];
+
+
+}
+
+
+
+
 
 
 //UsernamePasswordRef.update(usernamePasswordArray);
